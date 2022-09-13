b3985e3df283790e2135fe48cdbe349f50ada736
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-09-13 17:18:13 +0200

    Added `@since` now that the PR is accepted

--- src/Core/Notification/Action/Webhook/Exception/WebhookInvalidJsonValueException.php
+++ src/Core/Notification/Action/Webhook/Exception/WebhookInvalidJsonValueException.php
@@ -2,6 +2,9 @@
 
 namespace Combodo\iTop\Core\Notification\Action\Webhook\Exception;
 
+/**
+ * @since 1.1.2 N°5473 class creation
+ */
 class WebhookInvalidJsonValueException extends \CoreUnexpectedValue
 {
 
