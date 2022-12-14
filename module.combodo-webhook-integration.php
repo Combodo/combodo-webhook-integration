d8ca4922a033ef847dc4adf31619b25d74412eb5
Molkobain <lajarige.guillaume@free.fr>
2022-12-14 09:36:48 +0100

    Update module version to 1.1.2

--- module.combodo-webhook-integration.php
+++ module.combodo-webhook-integration.php
@@ -5,7 +5,7 @@
 //
 //
 SetupWebPage::AddModule(__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-webhook-integration/1.1.1', array(
+	'combodo-webhook-integration/1.1.2', array(
 		// Identification
 		//
 		'label' => 'Webhook integrations',
