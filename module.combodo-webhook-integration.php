5f338a9ecff282554fed74b4ac21ef71c37a0032
Stephen Abello <stephen.abello@combodo.com>
2024-01-08 16:35:10 +0100

    Update module version and dependencies

--- module.combodo-webhook-integration.php
+++ module.combodo-webhook-integration.php
@@ -5,7 +5,7 @@
 //
 //
 SetupWebPage::AddModule(__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-webhook-integration/1.3.3', array(
+	'combodo-webhook-integration/1.4.0', array(
 		// Identification
 		//
 		'label' => 'Webhook integrations',
@@ -14,10 +14,7 @@ SetupWebPage::AddModule(__FILE__, // Path to the current file, all other file na
 		// Setup
 		//
 		'dependencies' => array(
-			// Dependency to itop-config-mgmt should be v2.7.0, but we set it to v2.4.0 to enbale compatibility with some legacy iTop 2.7 customer packages
-			'itop-config-mgmt/2.4.0 || itop-structure/3.0.0',
-			// Dependency to this module is only here to force compatibility with iTop 2.7+, there is no functional dependency
-			'itop-config/2.7.0',
+			'itop-structure/3.2.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
