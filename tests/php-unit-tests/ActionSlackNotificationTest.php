bff244d68dc4f11d3642b2e2592ce951c81ce6c0
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2025-05-19 16:22:27 +0200

    N°8402 - fix(ActionSlackNotification): Improve TransformHTMLToSlackMarkup with… (#24)
    * fix(ActionSlackNotification): Improve TransformHTMLToSlackMarkup with current CKEditor
    
    * Add test to avoid future potential regressions on different tags (and also prevent the new line issue)
    
    ---------
    
    Co-authored-by: jf-cbd <jimmy.fayolle@combodo.com>

--- tests/php-unit-tests/ActionSlackNotificationTest.php
+++ tests/php-unit-tests/ActionSlackNotificationTest.php
@@ -53,6 +53,30 @@ HTML,
 HTML,
 				"expected" => "<!SomeGroup>",
 			],
+			"Simple p tags on different lines" => [
+				"input" => <<<HTML
+<p>hello</p><p>hi</p>
+HTML,
+				"expected" => "hello\nhi\n",
+			],
+			"Different tags on different lines" => [
+				"input" => <<<HTML
+<h2>hello</h2><p><strong>hi</strong></p>
+HTML,
+				"expected" => "*hello*\n*hi*\n",
+			],
+			"Nested tags" => [
+				"input" => <<<HTML
+<p><strong>I'm bold and</strong></p><p><i><strong>I'm bold and italic</strong></i></p>
+HTML,
+				"expected" => "*I'm bold and*\n_*I'm bold and italic*_\n",
+			],
+			"Large text with different tags" => [
+				"input" => <<<HTML
+<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><p><strong>Strong Text</strong><br><b>Bold Text</b><br><em>Emphasized Text</em><br><i>Italic Text</i><br><del>Deleted Text</del><br><s>Strikethrough Text</s><br><mark class="marker-yellow">Marked Text</mark><br><code>Inline Code</code></p><ul><li>List Item 1</li><li>List Item 2</li></ul><pre><code class="language-plaintext">Block of Code\r\nLine 1\r\nLine 2\r\n</code></pre>
+HTML,
+				"expected" => "*Heading 1*\n*Heading 2*\n*Heading 3*\n*Strong Text*\n*Bold Text*\n_Emphasized Text_\n_Italic Text_\n~Deleted Text~\n~Strikethrough Text~\n`Marked Text`\n`Inline Code`\n• List Item 1\n• List Item 2\n```Block of Code\r\nLine 1\r\nLine 2\r\n```\n",
+			],
 		];
 	}
 }
