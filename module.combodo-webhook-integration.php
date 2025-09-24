3c6fc74855f2daa8101dc12fba876110b58f5353
jf-cbd <jimmy.fayolle@combodo.com>
2025-09-24 17:58:35 +0200

    🔖 Update module version to 1.4.5

--- module.combodo-webhook-integration.php
+++ module.combodo-webhook-integration.php
@@ -5,7 +5,7 @@
 //
 //
 SetupWebPage::AddModule(__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-webhook-integration/1.4.4',
+	'combodo-webhook-integration/1.4.5',
 	array(
 		//
 		'label' => 'Webhook integrations',
